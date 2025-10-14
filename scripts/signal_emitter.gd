3527423234fd0820fe8101d37db0c97faf878f7e
Username4D <studio@kremzow.org>
2025-10-14 12:49:43 +0200

    Added passive signal emitters

--- scripts/signal_emitter.gd
+++ scripts/signal_emitter.gd
@@ -1,9 +1,16 @@
 extends Sprite2D
 
 var signal_scene = preload("res://scenes/signal.tscn")
+@export var passive = false
 
 func _input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_start"):
+	if Input.is_action_just_pressed("ui_start") and not passive:
+		var signal_obj = signal_scene.instantiate()
+		signal_obj.position = self.position
+		signal_obj.rotation = self.rotation
+		self.get_parent().add_child(signal_obj)
+func press():
+	if passive:
 		var signal_obj = signal_scene.instantiate()
 		signal_obj.position = self.position
 		signal_obj.rotation = self.rotation
