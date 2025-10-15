2de0f713102dacfedc2b6bf1f3a082b14eacb92e
Username4D <studio@kremzow.org>
2025-10-15 12:06:33 +0200

    added fmod to collision, added virtual rotation to avoid physics bugs, added rotator object that drags cells around it with it and turns when activated

---
+++ scripts/rotator.gd
@@ -0,0 +1,37 @@
+extends AnimatableBody2D
+
+@export var target_n: Node
+@export var target_o: Node
+@export var target_s: Node
+@export var target_w: Node
+
+@export var speed = 90
+
+var vrotation_degrees = 0
+
+var target_rotation = 0
+var og = 0
+func press():
+	target_rotation += 90
+	print("lol")
+func _physics_process(delta: float) -> void:
+	
+	print(self.vrotation_degrees)
+	vrotation_degrees = move_toward(vrotation_degrees, target_rotation, speed * delta)
+
+	for i in [target_n, target_o, target_s, target_w]:
+		
+		if i:
+	
+			i.rotation_degrees += vrotation_degrees - og
+	
+	if target_n:
+		target_n.global_position = $n.global_position
+	if target_o:
+		target_o.global_position = $o.global_position
+	if target_s:
+		target_s.global_position = $s.global_position
+	if target_w:
+		target_w.global_position = $w.global_position
+	og = vrotation_degrees 
+	self.rotation_degrees = vrotation_degrees
--- scripts/signal.gd
+++ scripts/signal.gd
@@ -2,34 +2,39 @@ extends CharacterBody2D
 
 const speed = 250
 
-func _physics_process(delta: float) -> void:
-	self.rotation_degrees = round(self.rotation_degrees)
+var rotation_virtual = 0
 
-	if self.rotation_degrees >= 360:
-		self.rotation_degrees -= 360
-	if self.rotation_degrees <0:
-		self.rotation_degrees += 360
-	velocity = Vector2(speed * delta, 0).rotated(self.rotation)
-	move_and_collide(velocity)
+func _physics_process(delta: float) -> void:
+	
+	velocity = Vector2(speed * delta, 0).rotated(deg_to_rad(rotation_virtual))
+	
+	
+	
 	if $ray.is_colliding():
 
 		if $ray.get_collider().is_in_group("wall_angled"):
-			print(round($ray.get_collider().rotation_degrees )== round(self.rotation_degrees))
-			print(round($ray.get_collider().rotation_degrees ), round(self.rotation_degrees))
-			if round($ray.get_collider().rotation_degrees )== round(self.rotation_degrees):
-				self.rotation_degrees += 90
-			elif round($ray.get_collider().rotation_degrees - 90) == round(self.rotation_degrees):
-				self.rotation_degrees += -90
+			
+			if fmod(round($ray.get_collider().rotation_degrees ), 360)== round(self.rotation_virtual):
+				self.rotation_virtual += 90
+			elif fmod(round($ray.get_collider().rotation_degrees ) + 270, 360) == round(self.rotation_virtual) or fmod(round($ray.get_collider().rotation_degrees ) - 90, 360) == round(self.rotation_virtual):
+				self.rotation_virtual += -90
 			else:
-				self.rotation_degrees += 180
+				self.rotation_virtual += 180
 			
 		
 		elif $ray.get_collider().is_in_group("button"):
 			$ray.get_collider().press()
-			self.rotation_degrees += 180
+			self.rotation_virtual += 180
 		else:
-			self.rotation_degrees += 180
-	self.rotation_degrees = round(self.rotation_degrees)
+			self.rotation_virtual += 180
+	self.rotation_virtual = round(self.rotation_virtual)
+	if self.rotation_virtual >= 360:
+		self.rotation_virtual -= 360
+	if self.rotation_virtual < 0.0:
+
+		self.rotate(deg_to_rad(360))
+	$ray.rotation_degrees = rotation_virtual
+	move_and_collide(velocity)
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_start"):
 		self.queue_free()
