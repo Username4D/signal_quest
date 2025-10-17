341a2e6b0f719b39005b67410d3fdf7b282fb32d
Username4D <studio@kremzow.org>
2025-10-17 18:52:12 +0200

    made shader colors into global variable to unify control, made shader easier to drop in by creating parameter for bg or foreground resulting in the fitting color pallete being choosen, added shader to all tiles/objects

--- scripts/gameplay_view.gd
+++ scripts/gameplay_view.gd
@@ -16,9 +16,15 @@ signal build
  "gated_button": 0,
  "toggle_wall": 0
 }
-
+@export var colors = [Color.HONEYDEW, Color.ALICE_BLUE, Color.ANTIQUE_WHITE, Color.CHOCOLATE]
+@export var colorsbg = [Color.HONEYDEW, Color.ALICE_BLUE, Color.ANTIQUE_WHITE, Color.CHOCOLATE]
 func _ready() -> void:
 	gameplay_handler.inventory = inventory
+	var ident = ["topleft", "topright", "bottomleft", "bottomright"]
+	for i in range(0,4):
+		RenderingServer.global_shader_parameter_set(ident[i], colors[i])
+	for i in range(0,4):
+		RenderingServer.global_shader_parameter_set(ident[i] + "bg", colorsbg[i])
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_start"):
