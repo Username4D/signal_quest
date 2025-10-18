9fd0e37d91c086de26b9768b2814170497ff6b17
Username4D <studio@kremzow.org>
2025-10-18 18:46:29 +0200

    added main menu

---
+++ scenes/main_menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var colors_schemes = []
+@export var colors_schemes_bg = []
+var rng = RandomNumberGenerator.new()
+
+func _ready() -> void:
+	rng.randomize()
+	var scheme_id = rng.randi_range(0, len(colors_schemes) - 1)
+	var scheme = colors_schemes[scheme_id]
+	var schemebg = colors_schemes_bg[scheme_id]
+	var ident = ["topleft", "topright", "bottomleft", "bottomright"]
+	for i in range(0,4):
+		RenderingServer.global_shader_parameter_set(ident[i], scheme[i])
+	for i in range(0,4):
+		RenderingServer.global_shader_parameter_set(ident[i] + "bg", schemebg[i])
