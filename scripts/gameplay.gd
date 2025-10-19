06d68f0fa4a815865670a616cc9ed2ff35fdd663
Username4D <studio@kremzow.org>
2025-10-19 21:35:39 +0200

    added resetting

--- scripts/gameplay.gd
+++ scripts/gameplay.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 @export var current_level = 1
+var level = null
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("esc"):
 		$pause_menu.visible = not $pause_menu.visible
+
+func reset():
+	level.queue_free()
+	var level_scene = load("res://scenes/levels/level_" + var_to_str(current_level) + ".tscn").instantiate()
+	self.add_child(level_scene)
+	level = level_scene
 
 func close():
 	$level.cose()
@@ -20,3 +27,4 @@ func finish():
 func _ready() -> void:
 	var level_scene = load("res://scenes/levels/level_" + var_to_str(current_level) + ".tscn").instantiate()
 	self.add_child(level_scene)
+	level = level_scene
