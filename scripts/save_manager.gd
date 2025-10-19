851db02da15a1822fdc114037c8d0f8f8029ee99
Username4D <studio@kremzow.org>
2025-10-19 12:45:42 +0200

    created savemanager

---
+++ scripts/save_manager.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var max_level = 0
+var sfx_volume = 100
+var high_contrast_mode = false
+
+func _ready() -> void:
+	var file = FileAccess.open("user://userdata.dat", FileAccess.READ)
+	if file != null:
+		var content = file.get_var()
+		max_level = content["max_level"]
+		sfx_volume = content["sfx_volume"]
+		high_contrast_mode = ["high_contrast_mode"]
+		file.close()
+func save():
+	var file = FileAccess.open("user://userdata.dat", FileAccess.WRITE)
+	file.store_var({"max_level": max_level, "high_contrast_mode": high_contrast_mode, "sfx_volume": sfx_volume})
+	file.close()
